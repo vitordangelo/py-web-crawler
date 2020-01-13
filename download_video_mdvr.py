83beeaf3e1fcabb0065150c401c65f0204d16ae2
Vitor <metroid.p2p@gmail.com>
2020-01-13 11:26:10 -0300

    object file video

--- download_video_mdvr.py
+++ download_video_mdvr.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "http://192.168.2.143/cgi-bin/VideoSearchOpr.cgi"
+url = "http://192.168.2.147/cgi-bin/VideoSearchOpr.cgi"
 
 payload = "-----011000010111000001101001\r\nContent-Disposition: form-data; \
     name=\"sdcard\"\r\n\r\n{}\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; \
@@ -17,39 +17,31 @@ payload = "-----011000010111000001101001\r\nContent-Disposition: form-data; \
     name=\"type\"\r\n\r\n{}\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; \
     name=\"channel\"\r\n\r\n{}\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; \
     name=\"select_type\"\r\n\r\n{}\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; \
-    name=\"Save\"\r\n\r\n{}\r\n-----011000010111000001101001--\r\n".format(1, 2019, 12, 12, 00, 00, 00, 23, 59, 59, 1, 1, 0, 'Search')
+    name=\"Save\"\r\n\r\n{}\r\n-----011000010111000001101001--\r\n".format(0, 2020, 1, 9, 00, 00, 00, 23, 59, 59, 1, 1, 0, 'Search')
 
 headers = {
     'content-type': 'multipart/form-data; boundary=---011000010111000001101001'}
 
-response = requests.post(url, data=payload, headers=headers)
+request = requests.post(url, data=payload, headers=headers)
 
-soup = BeautifulSoup(response.content, "html5lib")
+soup = BeautifulSoup(request.content, "html5lib")
 
-url_list = []
+files = []
 
-def list_url():
-    for link in soup.find_all('a'):
-        link = link.get('href')
-        url_list.insert(1, 'http://192.168.2.143' + link)
+soup = soup.find_all('li')
+for li in soup:
+    url = li.find('a').get('href')
+    file = li.get_text().split()
+    object = {'name': file[0], 'file_size': float(file[1]),
+              'date': file[3], 'time': file[4], 'path': url}
+    files.insert(1, object)
 
-def name_file(url):
-    file_name = url.split('/')
-    return file_name[5]
+def download(files):
+    for file in files:
+        file_path = "http://192.168.2.147{}".format(file['path'])
+        with open('/home/vitor/Documents/Apps/py-web-crawler/downloads/{}'.format(file['name']), 'wb') as f:
+            request = requests.get(file_path)
+            f.write(request.content)
+            print('File: {} complete'.format(file['name']))
 
-list_url()
-
-for url in url_list:
-    file_name = name_file(url)
-    with open('/home/vitor/Documents/Apps/py-web-crawler/downloads/'+file_name, 'wb') as f:
-        request = requests.get(url)
-        f.write(request.content)
-        print('File: {} complete'.format(url))
-
-        total = response.headers.get('content-length')
-        for data in request.iter_content(chunk_size=max(int(total/1000), 1024*1024)):
-            downloaded += len(data)
-            print(downloaded)
-
-
-print('TCHAU')
+download(files)
